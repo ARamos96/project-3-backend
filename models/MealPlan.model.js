0dd0b7e8ca380419859a885eee3f11dd69354d65
Arnau Ramos <arnauramos21@gmail.com>
2024-06-04 13:47:11 +0200

    added price and default values

--- models/MealPlan.model.js
+++ models/MealPlan.model.js
@@ -4,12 +4,19 @@ const mealPlanSchema = new mongoose.Schema({
   numberOfPeople: {
     type: Number,
     enum: [1, 2, 3, 4],
+    required: true,
+    default: 2,
   },
   numberOfServings: {
     type: Number,
     enum: [2, 3, 4, 5],
+    required: true,
+    default: 4,
   },
-
+  price: {
+    type: Number,
+    required: true,
+  },
 });
 
 const MealPlan = mongoose.model("MealPlan", mealPlanSchema);
