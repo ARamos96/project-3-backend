e7ce3c2ba85facf885be2ff4d4e74c0ab46152f1
guzegui <guzmanegui@gmail.com>
2024-06-06 20:39:58 +0200

    Add routes - mealplan, users, subscription

--- app.js
+++ app.js
@@ -24,6 +24,15 @@ app.use("/auth", authRoutes);
 const dishesRoutes = require("./routes/dishes.routes");
 app.use("/dishes", dishesRoutes);
 
+const mealPlanRoutes = require("./routes/mealplan.routes");
+app.use("/mealplan", mealPlanRoutes);
+
+const userRoutes = require("./routes/users.routes");
+app.use("/user", userRoutes);
+
+const subscriptionRoutes = require("./routes/subscription.routes");
+app.use("/subscription", subscriptionRoutes);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
---
+++ routes/mealplan.routes.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const router = express.Router();
+
+const { validateDish } = require("./../error-handling/dishes-errors");
+
+const roleValidation = require("../middleware/roleValidation");
+
+const MealPlan = require("../models/Dish.model");
+const { isAuthenticated } = require("../middleware/jwt.middleware");
+
+// GET all dishes
+router.get("/", (req, res, next) => {
+  Dish.find()
+    .then((dishes) => {
+      res.status(200).json(dishes);
+    })
+    .catch((err) => next(err));
+});
+
+// GET a single dish by ID
+router.get("/:id", (req, res, next) => {
+  Dish.findById(req.params.id)
+    .then((dish) => {
+      res.status(200).json(dish);
+    })
+    .catch((err) => next(err));
+});
+
+// POST a new dish
+router.post("/", validateDish, (req, res, next) => {
+  Dish.create(req.body)
+    .then((newDish) => {
+      res.status(201).json(newDish);
+    })
+    .catch((err) => next(err));
+});
+
+// PUT (replace) a dish by ID
+router.put(
+  "/:id",
+  isAuthenticated,
+  roleValidation(["admin"]),
+  (req, res, next) => {
+    const { id } = req.params;
+
+    Dish.findByIdAndUpdate(id, req.body, { new: true })
+      .then((updatedDish) => {
+        res.status(200).json(updatedDish);
+      })
+      .catch((err) => next(err));
+  }
+);
+
+// PATCH (update) a dish by ID
+router.patch(
+  "/:id",
+  isAuthenticated,
+  roleValidation(["admin"]),
+  (req, res, next) => {
+    const { id } = req.params;
+
+    Dish.findByIdAndUpdate(id, req.body, { new: true, runValidators: true })
+      .then((updatedDish) => {
+        res.status(200).json(updatedDish);
+      })
+      .catch((err) => next(err));
+  }
+);
+
+// DELETE a dish by ID
+router.delete(
+  "/:id",
+  isAuthenticated,
+  roleValidation(["admin"]),
+  (req, res, next) => {
+    const { id } = req.params;
+
+    Dish.findByIdAndDelete(id)
+      .then((deletedDish) => {
+        res.status(200).json(deletedDish);
+      })
+      .catch((err) => next(err));
+  }
+);
+
+module.exports = router;
