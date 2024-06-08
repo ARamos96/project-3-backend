f25b77c18af4858416a81f68d063ebfeda566b9b
guzegui <guzmanegui@gmail.com>
2024-06-08 16:38:48 +0200

    Add categories field

--- models/Dish.model.js
+++ models/Dish.model.js
@@ -2,12 +2,11 @@ const mongoose = require("mongoose");
 
 const { Schema, model } = mongoose;
 
-const { categoriesSchema } = require("./Categories.model");
-
 const dishSchema = new Schema({
   name: {
     type: String,
     required: true,
+    unique: true,
   },
   ingredients: {
     type: [String],
@@ -27,8 +26,67 @@ const dishSchema = new Schema({
     enum: ["easy", "medium", "hard"],
   },
   categories: {
-    type: categoriesSchema,
-    required: true,
+    origin: {
+      type: [String],
+      required: true,
+      enum: [
+        "Italian",
+        "Mexican",
+        "Indian",
+        "Turkish",
+        "Chinese",
+        "Japanese",
+        "French",
+        "American",
+        "MiddleEastern",
+        "Thai",
+        "Spanish",
+        "Greek",
+        "Korean",
+        "Vietnamese",
+      ],
+    },
+    diet: {
+      type: [String],
+      required: true,
+      enum: [
+        "Vegan",
+        "Vegetarian",
+        "Animal-protein",
+        "Pescatarian",
+        "Low-calories",
+        "High-protein",
+        "Keto",
+        "Paleo",
+        "Gluten-free",
+        "Dairy-free",
+      ],
+    },
+    cookingTime: {
+      type: String,
+      required: true,
+      enum: ["Fast", "Normal", "Slow"],
+    },
+    isHot: {
+      type: Boolean,
+      required: true,
+    },
+    allergens: {
+      type: [String],
+      required: true,
+      enum: [
+        "Eggs",
+        "Dairy",
+        "Wheat",
+        "Soy",
+        "Peanuts",
+        "Tree nuts",
+        "Fish",
+        "Shellfish",
+        "Sesame",
+        "None",
+      ],
+    },
   },
   steps: {
     type: [String],
