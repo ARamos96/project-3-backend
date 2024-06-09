978806deab397237655f8cbad7c39d3a3c68608d
guzegui <guzmanegui@gmail.com>
2024-06-09 20:52:46 +0200

    Destructure Address model from export Export router from route

--- routes/address.routes.js
+++ routes/address.routes.js
@@ -2,65 +2,65 @@ const roleValidation = require("../middleware/roleValidation");
 
 const express = require("express");
 
-const router = express.Router();
+// Destructure from the export at Address.model
+const { Address } = require("../models/Address.model");
 
-const Address = require("../models/Address.model");
+const router = express.Router();
 
 const { isAuthenticated } = require("../middleware/jwt.middleware");
 
 const User = require("../models/User.model");
 
-
 // Create a new Address with Post method
 
 router.post("/", (req, res, next) => {
-    Address.create(req.body)
-      .then((newAddress) => {
-        res.status(201).json(newAddress);
-      })
-      .catch((err) => next(err));
-  });
+  Address.create(req.body)
+    .then((newAddress) => {
+      res.status(201).json(newAddress);
+    })
+    .catch((err) => next(err));
+});
 
-  // Get all Addresses with Get method
-  router.get("/", (req, res, next) => {
-    Address.find()
-     .then((allAddresses) => {
-        res.status(200).json(allAddresses);
-      })
-     .catch((err) => next(err));
-  });
+// Get all Addresses with Get method
+router.get("/", (req, res, next) => {
+  Address.find()
+    .then((allAddresses) => {
+      res.status(200).json(allAddresses);
+    })
+    .catch((err) => next(err));
+});
 
-  //Get one address
-  router.get("/:id", (req, res, next) => {
-    Address.findById(req.params.id)
+//Get one address
+router.get("/:id", (req, res, next) => {
+  Address.findById(req.params.id)
     .populate(user)
     .populate(subscription)
 
-     .then((oneAddress) => {
-        res.status(200).json(oneAddress);
-      })
-     .catch((err) => next(err));
-  });
+    .then((oneAddress) => {
+      res.status(200).json(oneAddress);
+    })
+    .catch((err) => next(err));
+});
 
 // Edit address
-  router.put("/:id", (req, res, next) => {
-    Address.findByIdAndUpdate(req.params.id, req.body, { new: true })
-     .then((updatedAddress) => {
-        res.status(200).json(updatedAddress);
-      })
-     .catch((err) => next(err));
-  });
+router.put("/:id", (req, res, next) => {
+  Address.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    .then((updatedAddress) => {
+      res.status(200).json(updatedAddress);
+    })
+    .catch((err) => next(err));
+});
 
-  // Update address
-  router.patch("/:id", (req, res, next) => {
-    Address.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
-     .then((updatedAddress) => {
-        res.status(200).json(updatedAddress);
-      })
-     .catch((err) => next(err));
-  });
+// Update address
+router.patch("/:id", (req, res, next) => {
+  Address.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  })
+    .then((updatedAddress) => {
+      res.status(200).json(updatedAddress);
+    })
+    .catch((err) => next(err));
+});
 
-
-
-
-
+module.exports = router;
