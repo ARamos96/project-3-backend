c062893fd17df16d96d21904a4ed51c31def534c
guzegui <guzmanegui@gmail.com>
2024-06-10 16:54:26 +0200

    Add post subscription with references - no address or payment created independently

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const roleValidation = require("../middleware/roleValidation");
 
 const Subscription = require("../models/Subscription.model");
+const { Address } = require("../models/Address.model");
+const { Payment } = require("../models/PaymentMethod.model");
+const User = require("../models/User.model");
 const { isAuthenticated } = require("../middleware/jwt.middleware");
 
 // GET all subscriptions
@@ -27,6 +30,9 @@ router.get(
   roleValidation(["admin", "user"]),
   (req, res, next) => {
     Subscription.findById(req.params.id)
+      .populate("user")
+      .populate("mealplan")
+      .populate("dishes")
       .then((dish) => {
         res.status(200).json(dish);
       })
@@ -39,12 +45,35 @@ router.post(
   "/",
   isAuthenticated,
   roleValidation(["admin", "user"]),
-  (req, res, next) => {
-    Subscription.create(req.body)
-      .then((newSubscription) => {
-        res.status(201).json(newSubscription);
-      })
-      .catch((err) => next(err));
+  async (req, res, next) => {
+    // Destructure schemas to create separately
+    const { shippingAddress, paymentMethod, user } = req.body;
+
+    // try catch block
+    try {
+      const newSubscription = await Subscription.create(req.body);
+
+      // find user
+      const foundUser = await User.findById(user);
+
+      // Add references
+      newSubscription.shippingAddress.subscription = newSubscription._id;
+      newSubscription.shippingAddress.user = foundUser._id;
+      await newSubscription.shippingAddress.save();
+
+      newSubscription.paymentMethod.subscription = newSubscription._id;
+      newSubscription.paymentMethod.user = foundUser._id;
+      await newSubscription.paymentMethod.save();
+
+      foundUser.activeSubscription = newSubscription._id;
+      foundUser.paymentMethod = newSubscription.paymentMethod._id;
+      foundUser.address = newSubscription.shippingAddress._id;
+      await foundUser.save();
+
+      res.status(201).json(newSubscription);
+    } catch (err) {
+      next(err);
+    }
   }
 );
 
