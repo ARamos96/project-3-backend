c53c5acfc6bd8f34c09f7e8d289bd3a8f9d69ce8
guzegui <guzmanegui@gmail.com>
2024-06-12 10:01:33 +0200

    Import restrictedFields

--- routes/mealplan.routes.js
+++ routes/mealplan.routes.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const roleValidation = require("../middleware/roleValidation");
+const restrictedFields = require("../middleware/restrictedFields");
 
-const MealPlan = require("../models/MealPlan.model");
+const MealPlan = require("../models/MealPlan.model");z
 const { isAuthenticated } = require("../middleware/jwt.middleware");
 
 // GET all mealplans
