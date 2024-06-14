a35e4b65929b8e3fd5b577a9de91cdd413d2a5ee
guzegui <guzmanegui@gmail.com>
2024-06-14 23:21:55 +0200

    Typo in routes and error in import

--- routes/payment.routes.js
+++ routes/payment.routes.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User.model"); // Assuming User model is defined in models/User.js
-const PaymentMethod = require("../models/PaymentMethod.model"); // Assuming PaymentMethod model is defined in models/PaymentMethod.js
+const { Payment } = require("../models/PaymentMethod.model"); // Assuming PaymentMethod model is defined in models/PaymentMethod.js
 
 // Patch payment method route
-router.patch("/paymentMethod/:id", async (req, res, next) => {
+router.patch("/:id", async (req, res, next) => {
   const { id } = req.params;
   const changedFields = req.body;
 
   try {
     // Update the payment method
-    const updatedPaymentMethod = await updatePaymentMethod(id, changedFields);
+    const updatedPaymentMethod = await Payment.findByIdAndUpdate(
+      id,
+      changedFields
+    );
     res.status(200).json(updatedPaymentMethod);
   } catch (error) {
     next(error);
