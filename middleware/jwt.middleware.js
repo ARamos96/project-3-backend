93a0fedc88d61ecd1c822c2d545af23a78524181
guzegui <guzmanegui@gmail.com>
2024-07-02 12:36:05 +0200

    Add try catch block

--- middleware/jwt.middleware.js
+++ middleware/jwt.middleware.js
@@ -11,16 +11,19 @@ const isAuthenticated = jwt({
 // Function used to extract the JWT token from the request's 'Authorization' Headers
 function getTokenFromHeaders(req) {
   // Check if the token is available on the request Headers
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.split(" ")[0] === "Bearer"
-  ) {
-    // Get the encoded token string and return it
-    const token = req.headers.authorization.split(" ")[1];
-    return token;
+  try {
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.split(" ")[0] === "Bearer"
+    ) {
+      // Get the encoded token string and return it
+      const token = req.headers.authorization.split(" ")[1];
+      return token;
+    }
+  } catch (error) {
+    console.error("Error extracting token from headers:", error);
+    return null; // Return null if token extraction fails. This will prevent the request from being processed further.  // You might want to return a more specific error response here.
   }
-
-  return null;
 }
 
 // Export the middleware so that we can use it to create protected routes
